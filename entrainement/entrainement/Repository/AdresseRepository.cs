31173640c35ae65a4feb4c7b6767eebcc923e91c
Cedric <cedric.malaussena@gmail.com>
2020-03-25 12:45:26 +0100

    repo et crud
    l'art de ramer sur du sable

---
+++ entrainement/entrainement/Repository/AdresseRepository.cs
@@ -0,0 +1,48 @@
+﻿using entrainement.Domaine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace entrainement.Repository
+{
+    public class AdresseRepository : CrudRepository<Adresse>
+    {
+        private bddautoecoleContext context;
+        public AdresseRepository(bddautoecoleContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Adresse> FindAll()
+        {
+            //SELECT * FROM chercheur;
+            return this.context.Adresse.Select(AdresseRepository => adresse);
+        }
+
+        public IQueryable<Adresse> Filter(AdresseRepository model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Adresse FindByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Adresse> Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Adresse Save(Adresse model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Adresse Update(Adresse model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ entrainement/entrainement/Repository/Impl/EleveRepository.cs
@@ -0,0 +1,47 @@
+﻿using entrainement.Domaine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace entrainement.Repository.Impl
+{
+    public class EleveRepository
+    {
+        private bddautoecoleContext context;
+        public EleveRepository(bddautoecoleContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Eleves> FindAll()
+        {
+            return this.context.Eleves.Select(Eleves => eleve);
+        }
+
+        public IQueryable<Eleves> Filter(EleveRepository model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Eleves FindByID(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IQueryable<Eleves> Remove(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Adresse Save(Eleves model)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Adresse Update(Eleves model)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
